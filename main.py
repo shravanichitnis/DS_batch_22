62635cc5328a6bde599dd09a90957e601c1296ac
shravani <shravanichitnis01@gmail.com>
2025-08-02 18:06:00 +0530

    commited to dev branch

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("welcome to bug spotter")
     print("this is my dev branch")
+    print("Hello world")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
